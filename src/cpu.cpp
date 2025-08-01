4924e397043b822677c229e4961b238bcb1fe861
Caleb Hultmann <caleb.hultmann@gmail.com>
2025-07-31 23:53:00 -0500

    initial register work and group 2 opcode decoding

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -6,3 +6,92 @@ CPU::CPU() {
 CPU::~CPU() {
 
 }
+
+#define GROUP_0	0b00
+#define GROUP_1	0b01
+#define GROUP_2	0b10
+#define GROUP_3	0b11
+
+uint8_t& CPU::select_r8(uint8_t selector) {
+	switch (selector) {
+	case 0:
+		return B();
+	case 1:
+		return C();
+	case 2:
+		return D();
+	case 3:
+		return E();
+	case 4:
+		return H();
+	case 5:
+		return L();
+	case 6:
+		// NOT IMPLEMENTED YET, FETCH BYTE AT [HL]
+	case 7:
+		return A();
+	}
+}
+
+uint16_t& CPU::select_r16(uint8_t selector) {
+	switch (selector) {
+	case 0:
+		return BC();
+	case 1:
+		return DE();
+	case 2:
+		return HL();
+	case 3:
+		return SP();
+	}
+}
+
+uint16_t& CPU::select_r16stk(uint8_t selector) {
+	switch (selector) {
+	case 0:
+		return BC();
+	case 1:
+		return DE();
+	case 2:
+		return HL();
+	case 3:
+		return AF();
+	}
+}
+
+uint8_t opcode_group(uint8_t opcode) {
+	return (opcode | 0b11000000) >> 6;
+}
+
+void CPU::decode(uint8_t opcode) {
+	switch (opcode_group(opcode))
+	{
+	case GROUP_0:
+	case GROUP_1:
+
+	case GROUP_2: {
+		uint8_t operand = select_r8(opcode | 0b00000111);
+		switch ((opcode | 0b00111000) >> 3) {
+		case 0:
+			//add
+		case 1:
+			//adc
+		case 2:
+			//sub
+		case 3:
+			//sbc
+		case 4:
+			//and
+		case 5:
+			//xor
+		case 6:
+			//or
+		case 7:
+			//cp
+		}
+
+	}
+	case GROUP_3:
+
+	}
+}
--- src/cpu.h
+++ src/cpu.h
@@ -16,6 +16,7 @@ public:
 	CPU();
 	~CPU();
 
+private:
 	Reg16 AF;
 		// Flag bits 7: z, 6: n, 5: h, 4:c, 3210: unused
 	Reg16 BC;
@@ -23,4 +24,41 @@ public:
 	Reg16 HL;
 	uint16_t SP;
 	uint16_t PC;
+
+public:
+	uint8_t&  A()  { return AF.high; }
+	uint16_t& AF() { return AF.reg; }
+
+	uint8_t&  B()  { return BC.high; }
+	uint8_t&  C()  { return BC.low; }
+	uint16_t& BC() { return BC.reg; }
+
+	uint8_t&  D()  { return DE.high; }
+	uint8_t&  E()  { return DE.low; }
+	uint16_t& DE() { return DE.reg; }
+
+	uint8_t&  H()  { return HL.high; }
+	uint8_t&  L()  { return HL.low; }
+	uint16_t& HL() { return HL.reg; }
+
+	uint16_t& SP() { return SP; }
+	uint16_t& PC() { return PC; }
+
+private:
+
+	// First octal
+	// Second octal
+	//	  Similar to opcode_group()
+
+	void decode(uint8_t opcode);
+	uint8_t&  select_r8(uint8_t selector);
+	uint16_t& select_r16(uint8_t selector);
+	uint16_t& select_r16stk(uint8_t selector);
+	uint16_t& select_r16mem(uint8_t selector);
+	// cond
+	// b3?
+	// tgt3??
+	// imm8?
+	// imm16?
+
 };
