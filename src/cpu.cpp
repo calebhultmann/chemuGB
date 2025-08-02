377874d84ae406a2f40941e9933c500ed5a2adc7
Caleb Hultmann <caleb.hultmann@gmail.com>
2025-08-01 23:52:50 -0500

    more opcode decoding

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -60,7 +60,7 @@ uint16_t& CPU::select_r16stk(uint8_t selector) {
 }
 
 uint8_t opcode_group(uint8_t opcode) {
-	return (opcode | 0b11000000) >> 6;
+	return (opcode & 0b11000000) >> 6;
 }
 
 void CPU::decode(uint8_t opcode) {
@@ -68,10 +68,20 @@ void CPU::decode(uint8_t opcode) {
 	{
 	case GROUP_0:
 	case GROUP_1:
-
+		// 8-bit regsiter-to-register loads
+		if (opcode == 0b01110110) {
+			//halt
+		}
+		else {
+			// ld r8, r8
+			uint8_t destination = select_r8(opcode & 0b00111000 >> 3);
+			uint8_t source = select_r8(opcode & 0b00000111);
+			destination = source;
+		}
 	case GROUP_2: {
-		uint8_t operand = select_r8(opcode | 0b00000111);
-		switch ((opcode | 0b00111000) >> 3) {
+		// ___ a, r8
+		uint8_t operand = select_r8(opcode & 0b00000111);
+		switch ((opcode & 0b00111000) >> 3) {
 		case 0:
 			//add
 		case 1:
@@ -92,6 +102,34 @@ void CPU::decode(uint8_t opcode) {
 
 	}
 	case GROUP_3:
+		if (opcode & 0b00000111 == 0b110) {
+			// ___ a, imm8
+			switch ((opcode & 0b00111000) >> 3) {
+			case 0:
+				//add
+			case 1:
+				//adc
+			case 2:
+				//sub
+			case 3:
+				//sbc
+			case 4:
+				//and
+			case 5:
+				//xor
+			case 6:
+				//or
+			case 7:
+				//cp
+			}
+		}
+		else if (opcode & 0b11110111 == 0b11110011) {
+			((opcode >> 3) & 1) ? /* ei */ : /* di */;
+		}
+		else if (opcode & 0b00001011 == 0b0001) {
+			uint8_t reg = select_r16stk((opcode & 0b00111000) >> 3);
+			((opcode >> 2) & 1) ? /* pop reg */ : /* push reg */;
+		}
 
 	}
 }
