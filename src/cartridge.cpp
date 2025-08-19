8cfc5fddd7a842d6d9d1d7c71b014237b85ccb8c
Caleb Hultmann <caleb.hultmann@gmail.com>
2025-08-18 22:03:31 -0500

    initial cartridge read added

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -1,6 +1,8 @@
 #include "cartridge.h"
 #include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 Cartridge::Cartridge(const std::string& filename) {
 	struct gbHeader {
@@ -18,8 +20,57 @@ Cartridge::Cartridge(const std::string& filename) {
 		uint8_t	header_checksum;
 		uint8_t	global_checksum[2];
 	} header;
-	header = { 0 };
+
 	std::ifstream ifs;
 	std::filesystem::path filePath = std::filesystem::path(__FILE__).parent_path();
+	filePath /= "..\\roms";
+	filePath /= filename;
+	ifs.open(filePath, std::ifstream::binary);
+	if (!ifs.is_open()) {
+		// file not found
+		std::cout << "uh oh\n";
+		return;
+	}
+
+	ifs.seekg(0, ifs.end);
+	int len = int(ifs.tellg());
+	std::cout << "Length of rom: " << std::hex << len << "\n\n";
+
+	ifs.seekg(0x100);
+
+	ifs.read((char*)&header, sizeof(header));
+
+	std::cout << "Entry point: ";
+	for (int i = 0; i < 4; i++) {
+		std::cout << std::hex << int(header.entry_point[i]) << " ";
+	} std::cout << '\n';
+	std::cout << "Logo: ";
+	for (int i = 0; i < 48; i++) {
+		std::cout << std::hex << int(header.logo[i]) << " ";
+	} std::cout << '\n';
+	std::cout << "Title: ";
+	for (int i = 0; i < 16; i++) {
+		std::cout << std::hex << int(header.title[i]) << " ";
+	} std::cout << '\n';
+	std::cout << "Licensee code: ";
+	for (int i = 0; i < 2; i++) {
+		std::cout << std::hex << int(header.licensee_code[i]) << " ";
+	} std::cout << '\n';
+
+	std::cout << "SGB flag: " << std::hex << int(header.sgb_flag) << '\n';
+	std::cout << "Cartridge type: " << std::hex << int(header.cartridge_type) << '\n';
+	std::cout << "Rom size: " << std::hex << int(header.rom_size) << "\n";
+	std::cout << "Ram size: " << std::hex << int(header.ram_size) << "\n";
+	std::cout << "Destination: " << std::hex << int(header.destination) << "\n";
+	std::cout << "Old licensee code: " << std::hex << int(header.old_licensee_code) << "\n";
+	std::cout << "Version: " << std::hex << int(header.version) << "\n";
+	std::cout << "Header checksum: " << std::hex << int(header.header_checksum) << "\n";
+	std::cout << "Global checksum: ";
+	for (int i = 0; i < 2; i++) {
+		std::cout << std::hex << int(header.global_checksum[i]) << " ";
+	} std::cout << '\n';
+}
+
+Cartridge::~Cartridge() {
 
 }
--- src/cartridge.h
+++ src/cartridge.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 class Cartridge
 {
@@ -7,5 +8,36 @@ public:
 	Cartridge(const std::string& filename);
 	~Cartridge();
 
+	std::vector<std::vector<uint8_t>> romBanks; // 0x4000 per bank
 
+
+	//rom, external ram, mbc
+
+	// Cartridge Types
+	//$00	ROM ONLY
+	//$01	MBC1
+	//$02	MBC1 + RAM
+	//$03	MBC1 + RAM + BATTERY
+	//$05	MBC2
+	//$06	MBC2 + BATTERY
+	//$0B	MMM01
+	//$0C	MMM01 + RAM
+	//$0D	MMM01 + RAM + BATTERY
+	//$0F	MBC3 + TIMER + BATTERY
+	//$10	MBC3 + TIMER + RAM + BATTERY 10
+	//$11	MBC3
+	//$12	MBC3 + RAM 10
+	//$13	MBC3 + RAM + BATTERY 10
+	//$19	MBC5
+	//$1A	MBC5 + RAM
+	//$1B	MBC5 + RAM + BATTERY
+	//$1C	MBC5 + RUMBLE
+	//$1D	MBC5 + RUMBLE + RAM
+	//$1E	MBC5 + RUMBLE + RAM + BATTERY
+	//$20	MBC6
+	//$22	MBC7 + SENSOR + RUMBLE + RAM + BATTERY
+	//$FC	POCKET CAMERA
+	//$FD	BANDAI TAMA5
+	//$FE	HuC3
+	//$FF	HuC1 + RAM + BATTERY
 };
